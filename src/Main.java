3d30e1fdb4bc6f47635166df1f096496f62bc236
Pedro <116972289+pedr0albuquerque@users.noreply.github.com>
2024-10-10 21:22:43 -0300

    inserindo produtos no bd

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         cadastroVIEW cadView = new cadastroVIEW();
         cadView.setVisible(true);
+        ProdutosDAO prodDAO = new ProdutosDAO();
+        
+        prodDAO.cadastrarProduto();
     }
     
 }
--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +23,28 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    
+    public void cadastrarProduto (){
         
-        
-        conn = new conectaDAO().connectDB();
-        
-        
+        try{
+            String sql = "INSERT INTO produtos(nome,valor,status) VALUES(?,?,?)";
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            
+            /*
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            */
+            
+            prep.setString(1, "iphone");
+            prep.setInt(2, 4000);
+            prep.setString(3, "vendido");
+            prep.executeUpdate();
+        }catch(SQLException ex){
+            ex.getMessage();
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
